171890a32e9034bb655f434a59fc53dca6528902
Visfire <visfire12@gmail.com>
2019-08-04 20:57:32 -0400

    added actual html

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+var path = require('path');
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
